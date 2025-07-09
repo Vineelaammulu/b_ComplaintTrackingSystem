bd81ee34632bfd5bd0238cc6daa3509057365701
bhudati vineela <vineelabhudati@gmail.com>
2025-07-09 23:10:26 +0530

    initial-commit

---
+++ src/main/java/org/cts/app/CtsApplication.java
@@ -0,0 +1,11 @@
+package org.cts.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CtsApplication.class,args);
+    }
+}
